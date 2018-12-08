38ca894c8186c6c2febaf9833ada92fb043ae555
Xinyi <xchen92@u.rochester.edu>
2018-12-09 05:52:21 +0800

    ..

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php include "inc/html-top.inc"; ?>
 
-<body>
 
-<header class="persistent">
+
+
+<body class="z-pattern">
+<div class="container">
+
+<header class="persistent primary">
 	<h1>Rugby</h1>
 </header>
+
 <?php include "inc/nav.inc";?> 
-<figure>
-	<img class="hero" src="images/rugbyball.png" alt="rugby ball">
-</figure>
+
 <main class="about">
 <p>
 	A team game played with an oval ball that may be kicked, carried, and passed from hand to hand. Points are scored by grounding the ball behind the opponents' goal line (thereby scoring a try) or by kicking it between the two posts and over the crossbar of the opponents' goal.
 </p>
 </main>
+
 <section class="terminal">
 	<div class="learnmore">
 Learn More!
 	</div>
 <section>
+	
+</div><!-- .container -->
 
 
 <?php include "inc/js.inc"; ?>
